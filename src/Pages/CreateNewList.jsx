02c928cbf49fe5cb590a7e1dbb11c9399ea56a30
JoseCoelho25 <zenani87@hotmail.com>
2023-02-05 18:13:06 +0000

    createlistpage layout

--- src/Pages/CreateNewList.jsx
+++ src/Pages/CreateNewList.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import List from "../assets/imgs/shopping-list.png";
 
 function CreateNewList() {
   return (
-    <div>CreateNewList</div>
+    <div className="max-w-screen-2xl mx-auto">
+      <h1 className="text-center text-4xl mt-36 font-bold">
+        Create New List
+      </h1>
+      <div className="flex relative">
+        <img src={List} alt="list" className="absolute w-8 inset-x-20 inset-y-11" />
+        <input type="text" name="Enter list name" id="oi" placeholder='Enter list name' className="mt-10 p-2 pl-12 flex mx-auto  rounded-lg  shadow-lg shadow-slate-400" />
+      </div>
+      <div className="flex justify-center space-x-12 mt-20">
+          <input 
+          type="button" 
+          value="Cancel" 
+          className="bg-gray-500 text-white rounded-full px-4 py-2 text-xl" />
+          <input 
+          type="button" 
+          value="Create" 
+          className="text-xl bg-gradient-to-r from-[#F26C6D] to-[#C65757] px-4 py-2 rounded-full text-white flex"/>
+        </div>
+      
+      
+    </div>
   )
 }
 
