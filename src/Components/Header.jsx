255f036505b573e42051593d9cee75dfdca7c874
JoseCoelho25 <zenani87@hotmail.com>
2023-02-10 14:59:53 +0000

    sync hamburguer menu

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 function Header( props) {
     const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="flex bg-gradient-to-r from-[#f3977a] to-[#ef4876] pb-2">
+    <div className="absolute w-screen z-20 flex bg-gradient-to-r from-[#f3977a] to-[#ef4876] pb-2">
             <div className="w-1/4">
             <button className="w-full" onClick={() => setIsOpen(!isOpen)}>
             {isOpen ? 
@@ -12,7 +12,7 @@ function Header( props) {
             : 
             <div className="text-2xl">&#9776;</div>}
             </button>
-            {isOpen && <div className=" grid grid-cols-1">
+            {isOpen && <div className=" mx-auto w-screen flex justify-around">
                 <Link to="/" className="hover:bg-blue-400 rounded-lg p-1 ">Lists</Link>
                 <Link to="/recipes" className="hover:bg-blue-400 rounded-lg p-1">Recipes</Link>
                 <Link to="/account" className="hover:bg-blue-400 rounded-lg p-1">My account</Link>
